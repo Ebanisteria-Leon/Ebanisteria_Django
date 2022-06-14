02dc3113a5682cc82524e9b79206245f8fdafe4e
Gohan-117holguin <gojan.1407holguin@gmail.com>
2022-06-14 17:17:10 -0500

    Promo serializers

--- products/api/serializer/promo_serializers.py
+++ products/api/serializer/promo_serializers.py
@@ -1,38 +1,12 @@
+from api.serializer.product_serializers import ProductoSerializers
 from products.models import Promocion
 
 from rest_framework import serializers
 
 class PromocionSerializers(serializers.ModelSerializer):
+    idProducto = ProductoSerializers(many = True)
+    
     class Meta:
         model = Promocion
+        fields = '__all__'
         exclude = ('estadoCreacion', 'fechaCreacion', 'fechaModificacion', 'fechaEliminacion')
-    
-    def validate_product(self, value):
-        if value == '' or value == None:
-            raise serializers.ValidationError('Debe ingresar un Producto')
-        
-        return value
-    
-    def validate(self, data):
-        if 'idProducto' not in data.keys():
-            raise serializers.ValidationError({
-                    'idProducto': 'Debe ingresar un Producto'
-                })
-        
-        return data
-    
-    def to_representation(self, instance):
-        return {
-            'idPromocion': instance.idPromocion,
-            'nombre': instance.nombre,
-            
-            'valorDescuento': instance.valorDescuento,
-            'productoExtra': instance.productoExtra,
-            
-            'estadoProducto': instance.estadoProducto,
-            'idProducto': [
-                instance.idProducto.nombre,
-                instance.idProducto.descripcion,
-                instance.idProducto.valor,
-            ]
-        }
